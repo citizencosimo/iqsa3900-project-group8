b93b27d1e0c637e06cc7e34a2402c296dea73e8f
citizencosimo <61855193+citizencosimo@users.noreply.github.com>
2023-03-20 14:16:59 -0500

    Fixed error

--- accounts/models.py
+++ accounts/models.py
@@ -8,8 +8,10 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=100)
     is_onprobation = models.BooleanField(default=False,
                                          help_text="Designates whether this user in on probation for innapropriate conduct.",
-                                         verbose_name="probation status")
-    moderation_message = models.CharField(max_length=1000, help_text="Explanation of why the use has been put on probation or banned.", blank=True, null=True)
+                                         verbose_name="probation status", null=True)
+    moderation_message = models.CharField(max_length=1000,
+                                          help_text="Explanation of why the use has been put on probation or banned.",
+                                          blank=True, null=True)
     def __str__(self):
         return self.username
 # Create your models here.
