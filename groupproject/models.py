c2939acc2c6f2690dac018891f5a1f7fa5d5fbc9
mulvey29 <mulvey29@gmail.com>
2023-03-21 22:24:27 +0100

    Created Platform model

--- groupproject/models.py
+++ groupproject/models.py
@@ -5,3 +5,12 @@ from django.db import models
 
 
 # Create your models here.
+class Platform(models.Model):
+    platform_id = models.AutoField(primary_key=True)
+    platform_name = models.CharField(max_length=25)
+    platform_image = models.ImageField(upload_to='platform_images/')
+    platform_description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.platform_name
+
