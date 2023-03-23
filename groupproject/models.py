89eadf26a06bd188932dcf4759145678fc02d31f
citizencosimo <61855193+citizencosimo@users.noreply.github.com>
2023-03-22 23:49:06 -0500

    Moved publisher class to groupproject\models.py "merged" developer into this branch

--- groupproject/models.py
+++ groupproject/models.py
@@ -10,6 +10,23 @@ class Genre(models.Model):
     genre_name = models.CharField(max_length=200, help_text="The genre(s) of the game.")
     genre_description = models.CharField(max_length=200, blank="True", null="True")
 
+class Publisher(models.model):
+    publisher_id = models.IntegerField(max_length=8, primary_key=True)
+    publisher_name = models.CharField(max_length=1000)
+    publisher_image = models.ImageField(upload_to='images/', null=True, blank=True)
+    publisher_country = models.CharField(max_length=500)
+    publisher_description = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.publisher_name
+
+class Developer(models.Model):
+    developer_id = models.Autofield(primary_key=True)
+    developer_name = models.CharField(max_length=1000)
+    developer_image = models.ImageField(upload_to='images/')
+    developer_country = models.CharField(max_length=500)
+    developer_description = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.developer_name
 
 class Platform(models.Model):
     platform_id = models.AutoField(primary_key=True)
