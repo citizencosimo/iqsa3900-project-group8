3a1252768116f226470aca98cf58a372c33f3cac
kfritts0409 <kfritts09@gmail.com>
2023-03-22 22:35:31 -0500

    Developer Model
    The class made in models.py for groupproject

--- accounts/models.py
+++ accounts/models.py
@@ -13,3 +13,13 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 # Create your models here.
+
+class Developer(models.Model)
+    Developer_ID = models.Autofield(primary_key=True)
+    Developer_Name = models.CharField(max_length=1000)
+    Developer_Image = models.ImageField(upload_to='images/')
+    Developer_Country = models. CharField(max_length=500)
+    Developer_Description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.Developer_Name
