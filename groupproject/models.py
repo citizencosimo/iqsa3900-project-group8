b9a4982908888cc6dfd3a747ce4ebb0360144d76
citizencosimo <61855193+citizencosimo@users.noreply.github.com>
2023-03-25 09:49:05 -0500

    First pass at Game class

--- groupproject/models.py
+++ groupproject/models.py
@@ -4,14 +4,13 @@ from accounts import models
 from django.db import models
 
 class Language(models.Model):
-    language_name = models.CharField(max_length=25, default="Unknown")
-    language_charset = models.CharField(max_length=25, default="Unknown", help_text="The alphabet used by the language. Currently unused")
+    name = models.CharField(max_length=25, default="Unknown")
+    charset = models.CharField(max_length=25, default="Unknown", help_text="The alphabet used by the language. Currently unused")
 class Genre(models.Model):
-    genre_name = models.CharField(max_length=200, help_text="The genre(s) of the game.")
-    genre_description = models.CharField(max_length=200, blank="True", null="True")
+    name = models.CharField(max_length=200, help_text="The genre(s) of the game.")
+    description = models.CharField(max_length=200, blank="True", null="True")
 
-class Publisher(models.model):
-    publisher_id = models.IntegerField(max_length=8, primary_key=True)
+class Publisher(models.Model):
     publisher_name = models.CharField(max_length=1000)
     publisher_image = models.ImageField(upload_to='images/', null=True, blank=True)
     publisher_country = models.CharField(max_length=500)
@@ -20,20 +19,63 @@ class Publisher(models.model):
         return self.publisher_name
 
 class Developer(models.Model):
-    developer_id = models.Autofield(primary_key=True)
     developer_name = models.CharField(max_length=1000)
-    developer_image = models.ImageField(upload_to='images/')
+    developer_image = models.ImageField(upload_to='images/', blank=True, null=True)
     developer_country = models.CharField(max_length=500)
     developer_description = models.CharField(max_length=1000)
     def __str__(self):
         return self.developer_name
 
 class Platform(models.Model):
-    platform_id = models.AutoField(primary_key=True)
     platform_name = models.CharField(max_length=25)
-    platform_image = models.ImageField(upload_to='platform_images/')
+    platform_image = models.ImageField(upload_to='images/', blank=True, null=True)
     platform_description = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.platform_name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    release_date = models.DateField(null=True, blank=True)
+
+    developer = models.ForeignKey('Developer', on_delete=models.RESTRICT, null=True)
+    publisher = models.ForeignKey('Publisher', on_delete=models.RESTRICT, null=True)
+    platform = models.ForeignKey('Platform', on_delete=models.RESTRICT, null=True)
+
+    language = models.ManyToManyField(Language, related_name='language')
+    genre = models.ManyToManyField(Genre, related_name='language')
+
+    UNRATED = 'UR'
+    EVERYONE = 'EO'
+    EVERYONEPLUS = 'E+'
+    TEEN = 'TE'
+    MATURE = 'MA'
+    ADULT = 'AO'
+
+    RATING_LEVELS = [
+        (UNRATED, 'Unrated'),
+        (EVERYONE, 'Everyone'),
+        (EVERYONEPLUS, 'Everyone 10+'),
+        (TEEN, 'Teen'),
+        (MATURE, 'Mature'),
+        (ADULT, 'Adults Only')
+    ]
+    rating = models.CharField(
+        max_length = 2,
+        choices = RATING_LEVELS,
+        default = UNRATED
+    )
+
+    description = models.TextField(max_length=1000, help_text='Enter a brief description of the game')
+
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['title', 'platform', 'release_date']
+
+    def __str__(self):
+        return self.title + ' (' + self.platform + ', ' + self.release_date.year + ')'
+
+
 
