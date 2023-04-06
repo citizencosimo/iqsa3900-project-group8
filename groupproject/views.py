82adb1c19ec25835a85ba96a0b3d5e813091cc19
citizencosimo <61855193+citizencosimo@users.noreply.github.com>
2023-04-06 10:06:08 -0500

    Added logic for Developer and Publisher

--- groupproject/views.py
+++ groupproject/views.py
@@ -4,7 +4,7 @@ from .forms import PublisherForm, GameForm, DeveloperForm, GenreForm, PlatformFo
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.http import HttpResponse
-from groupproject.models import Game
+from groupproject.models import Game, Publisher, Developer, Platform, Genre, Language
 from django.urls import reverse_lazy
 
 
@@ -92,7 +92,66 @@ def DeleteGame(request, pk, template_name='data/game/delete.html'):
     return render(request, template_name, context)
 
 def GameList(request, template_name='data/game_list.html'):
-    games = Game.objects.all()
+    gamess = Game.objects.all()
     data = {}
     data['objects_list'] = games
+    return render(request, template_name, data)
+
+def PublisherList(request, template_name='data/publisher_list.html'):
+    publishers = Publisher.objects.all()
+    data = {}
+    data['objects_list'] = publishers
+    return render(request, template_name, data)
+
+def ViewPublisher(request, pk, template_name='data/publisher/publisher.html'):
+    context={}
+    publisher=get_object_or_404(Publisher, pk=pk)
+    context['publisher'] = publisher
+    return render(request, template_name, context)
+
+def UpdatePublisher(request, pk, template_name='data/publisher/update.html'):
+    context = {}
+    publisher = get_object_or_404(Publisher, pk=pk)
+    context['publisher'] = publisher
+    form = PublisherForm(request.POST or None, instance=publisher)
+    context['form'] = form
+    if form.is_valid():
+        form.save()
+        return redirect('publisher_list')
+    return render(request, template_name, context)
+
+
+def DeletePublisher(request, pk, template_name='data/publisher/delete.html'):
+    context={}
+    context['publisher'] = get_object_or_404(Publisher, pk=pk)
+    return render(request, template_name, context)
+
+
+def ViewDeveloper(request, pk, template_name='data/developer/developer.html'):
+    context={}
+    developer=get_object_or_404(Developer, pk=pk)
+    context['developer'] = developer
+    return render(request, template_name, context)
+
+def UpdateDeveloper(request, pk, template_name='data/developer/update.html'):
+    context = {}
+    developer = get_object_or_404(Publisher, pk=pk)
+    context['developer'] = developer
+    form = DeveloperForm(request.POST or None, instance=developer)
+    context['form'] = form
+    if form.is_valid():
+        form.save()
+        return redirect('developer_list')
+    return render(request, template_name, context)
+
+
+def DeleteDeveloper(request, pk, template_name='data/developer/delete.html'):
+    context={}
+    context['developer'] = get_object_or_404(Developer, pk=pk)
+    return render(request, template_name, context)
+
+def DeveloperList(request, template_name='data/developer_list.html'):
+    developers = Developer.objects.all()
+    data = {}
+    data['objects_list'] = developers
     return render(request, template_name, data)
