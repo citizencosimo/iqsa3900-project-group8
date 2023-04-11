159dbdd7cd4020322770d62d79f32e61b1ac1358
citizencosimo <61855193+citizencosimo@users.noreply.github.com>
2023-04-11 17:12:27 -0500

    Created usertools and reviewertools app

--- group3900/settings.py
+++ group3900/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'groupproject.apps.GroupprojectConfig',
+    'usertools.apps.UsertoolsConfig',
     'accounts',
     'search.apps.SearchConfig',
 
