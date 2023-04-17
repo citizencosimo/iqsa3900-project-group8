ea93d0d89ee1f9677e3339e24fe44f61310d7e84
kfritts0409 <kfritts09@gmail.com>
2023-04-16 23:29:06 -0500

    Added edit profile functionality and fixed drop down feature for user name

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,21 @@
 # accounts/views.py
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import CustomUserCreationForm
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = CustomUser
+    form_class = CustomUserChangeForm
+    success_url = reverse_lazy('profile_update_done')
+    template_name = 'accounts/profile_update.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
