13ffcf5062c8078abbdef0bca6d5a6bb143e4c25
citizencosimo <61855193+citizencosimo@users.noreply.github.com>
2023-04-26 10:52:10 -0500

    Reviews editable

--- reviewertools/urls.py
+++ reviewertools/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('', views.ReviewIndexTest, name='review_index'),
-    path('write_review/<int:game_id>', views.CreateReview, name='write_review')
+    path('write_review/<int:game_id>', views.CreateReview, name='write_review'),
+    path('update_review/<uuid:review_id>', views.UpdateReview, name='update_review'),
 ]
--- reviewertools/views.py
+++ reviewertools/views.py
@@ -1,7 +1,10 @@
+from datetime import time, datetime
+
 from reviewertools.models import Review
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ReviewForm
 from groupproject.forms import Game
+from django.contrib import messages
 
 def ReviewIndexTest(request):
         context = {}
@@ -25,5 +28,30 @@ def CreateReview(request, game_id):
                 print('failure')
 
         return render(request, 'review/view.html', context)
+
+
+def UpdateReview(request, review_id):
+        context = {}
+        review = get_object_or_404(Review, pk=review_id)
+        if request.user.pk == review.user.pk:
+                # game= review.game
+                form = ReviewForm(request.POST or None, request.FILES or None, instance = review)
+                context['form'] = form
+                if form.is_valid():
+                        # For later, to save an updated date
+                        # edit_time = datetime.now().strftime("%d-%m-%Y at %H:%M:%S")
+                        # form.instance.comment += "Edited on {}".format(edit_time);
+                        cleaned_data = form.cleaned_data
+                        form.save(cleaned_data)
+                        messages.success(request, 'Successfully Edited')
+                        return redirect('game_view', pk=review.game.pk)
+                return render(request, 'review/view.html', context)
+
+        else:
+                messages.error(request, "Cannot edit: You are not the author.")
+                return redirect('game_view', pk=review.game.pk)
+
+
+
 
 
