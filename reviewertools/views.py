bb16cad18e32cc075e86535c632baeceb8189cf5
citizencosimo <61855193+citizencosimo@users.noreply.github.com>
2023-04-26 08:41:09 -0500

    Review form functional.

--- reviewertools/views.py
+++ reviewertools/views.py
@@ -9,8 +9,21 @@ def ReviewIndexTest(request):
 
 def CreateReview(request, game_id):
         context = {}
-        context["form"] = ReviewForm(request.POST or None, request.FILES or None)
-        context["game"] = get_object_or_404(Game, pk=game_id)
+        game = get_object_or_404(Game, pk=game_id)
+        context["game"] = game
+
+        form = ReviewForm(request.POST or None, request.FILES or None)
+        context["form"] = form
+
+        if form.is_valid():
+                form.instance.game = game
+                form.instance.user = request.user
+                cleaned_data = form.cleaned_data
+                form.save(cleaned_data)
+
+        else:
+                print('failure')
+
         return render(request, 'review/view.html', context)
 
 
