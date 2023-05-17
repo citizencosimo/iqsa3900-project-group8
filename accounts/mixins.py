7f3ba15c2dfa062963572f659c10d300043f7aca
citizencosimo <61855193+citizencosimo@users.noreply.github.com>
2023-05-17 12:00:15 -0500

    Added mixin module

---
+++ accounts/mixins.py
@@ -0,0 +1,9 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class StaffRequiredMixin:
+    def dispatch(self, request):
+        print(request.user.is_staff)
+        if not request.user.is_staff:
+            redirect('home')
+        return True
